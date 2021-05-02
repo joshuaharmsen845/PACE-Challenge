9b8738ef28eaf74ecef79240c22756eb96b636d0
Joshua Harmsen <57068824+joshuaharmsen845@users.noreply.github.com>
2021-05-02 08:39:20 -0400

    Added questions

--- large_file.cpp
+++ large_file.cpp
@@ -23,9 +23,9 @@ void break_it(graph graph, unordered_set cluster) {
   for (size_t i = 0; i < cluster.size(); i++) {
     unordered_set neighbors = graph[i];
     for (size_t j = i + 1; j < neighbors.size(); j++) {
-      for (size_t k = 0; k < cluster.size(); k++) {
+      for (size_t k = 0; k < cluster.size(); k++) {       //Does this cut E/2 edges or all of them? We can get away with E/2.
         if (cluster[k] == neighbors[j])
-          graph.cutEdge(i, j);
+          graph.cutEdge(i, j);              
       }
     }
 }
@@ -35,8 +35,8 @@ void large_file(graph graph) {
   for (size_t i = 0; i < graph.size(); i++) {
     float con = 0;
     unordered_set cluster = graph.findCluster(i);
-    int max_con = cluster.size() * (cluster.size() - 1)
-    for (size_t j = 0; j < cluster.size(); j++)
+    int max_con = cluster.size() * (cluster.size() - 1) //If we calculate this, why use connectedness?
+    for (size_t j = 0; j < cluster.size(); j++)         //We know the number of edges, its an attribute of the Graph class. 
       con += graph.connectedness(j)
     float con_per = con / con_max
     if (con_per < .5)

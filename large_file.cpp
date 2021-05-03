fc8e1c861e467d90984ad535931a942bd09f0b98
ajzucker <70109530+ajzucker@users.noreply.github.com>
2021-05-03 14:35:05 -0400

    Update large_file.cpp
    Things you asked to fixed should be done

--- large_file.cpp
+++ large_file.cpp
@@ -1,47 +1,50 @@
-#include <iostream>
-#include <fstream>
-#include <unordered_set>
-#include <map>
-#include "graph.h"
-
-void clique_it(graph graph, unordered_set cluster) {
-  for (size_t i = 0; i < cluster.size(); i++) {
-    unordered_set neighbors = graph[i];
-    unordered_set not_nei = cluster;
-    for (size_t j = i + 1; j < neighbors.size(); j++) {
-      for (size_t k = 0; k < cluster.size(); k++) {
-        if (cluster[k] == neighbors[j])
-          not_nei.remove(cluster[k]);
+void Graph::clique_it(std::unordered_set<int> cluster) {
+  for (auto i: cluster) {
+    std::unordered_set<int> neighbors = graph[i];
+    std::unordered_set<int> not_nei = cluster;
+    for (auot j: neighbor) {
+      for (auto k: cluster) {
+        if (k == j)
+          not_nei.erase(k);
       }
     }
-    for (size_t l = 0; l < not_nei.size(); l++)
-      graph.addEdge(i, l);
+    for (auto l: not_nei)
+      addEdge(i, l);
   }
 }
 
-void break_it(graph graph, unordered_set cluster) {
-  for (size_t i = 0; i < cluster.size(); i++) {
-    unordered_set neighbors = graph[i];
-    for (size_t j = i + 1; j < neighbors.size(); j++) {
-      for (size_t k = 0; k < cluster.size(); k++) {       //Does this cut E/2 edges or all of them? We can get away with E/2.
-        if (cluster[k] == neighbors[j])
-          graph.cutEdge(i, j);              
-      }
+void Graph::break_it(std::unordered_set<int> cluster) {
+  for (auto i: cluster) {
+    std::unordered_set<int> neighbors = graph[i];
+    for (auto j: neighbor) {
+      for (auto k: cluster)        //Does this cut E/2 edges or all of them? We can get away with E/2.
+        if (k == j) //Cannot iterate
+          cutEdge(i, j);
     }
-}
-
-
-void large_file(graph graph) {
-  for (size_t i = 0; i < graph.size(); i++) {
-    float con = 0;
-    unordered_set cluster = graph.findCluster(i);
-    int max_con = cluster.size() * (cluster.size() - 1) //If we calculate this, why use connectedness?
-    for (size_t j = 0; j < cluster.size(); j++)         //We know the number of edges, its an attribute of the Graph class. 
-      con += graph.connectedness(j)
-    float con_per = con / con_max
-    if (con_per < .5)
-      clique_it(graph, cluster);
-    else
-      break_it(graph, cluster);
   }
 }
+
+void Graph::large_file() {
+  std:: unordered_set<int> used = []
+  for (auto i: graph) { 
+    float con = 0;                            
+    bool iter = false;
+    std::unordered_set<int> cluster = findCluster(i);
+    int max_con = cluster.size() * (cluster.size() - 1);
+    for (auto j: cluster)
+      con += connectedness(j);
+    float con_per = con / max_con;
+    for (auto j: cluster) {
+      for (auto k: used) {
+        if (j == k)
+          iter = true;
+      }
+    }
+    if !iter
+      if (con_per < .5)
+        clique_it(cluster);
+      else
+        break_it(cluster);
+  }
+}
+
